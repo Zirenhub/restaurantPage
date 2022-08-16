56a3161433296eb75080e656be52be15e974d95e
Erdinch <erdinchorhan02@gmail.com>
2022-08-16 14:51:06 +0300

    Initial webpack commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // use for if more than one entry points.
+  //   optimization: {
+  //     runtimeChunk: 'single',
+  //   },
+};
