de45c509aa8f0658d7a85bbd1e941b7ba42d8cb3
Erdinch <erdinchorhan02@gmail.com>
2022-08-17 19:24:40 +0300

    initial main page

--- src/app/page-load.js
+++ src/app/page-load.js
@@ -1,87 +1,93 @@
 const template = document.querySelector('#content');
 
-////////////////////////////
-//          HEADER        //
-////////////////////////////
-
-//header
-const header = document.createElement('header');
-header.classList.add('header-container');
-
-//header, content
-const headerContent = document.createElement('div');
-headerContent.classList.add('header-content');
-
-//header welcome to text
-const headerText = document.createElement('p');
-headerText.textContent = 'Welcome to';
-
-//header mini text
-const headerMiniText = document.createElement('p');
-headerMiniText.textContent =
-  'Where something good is always cooking.';
-
-//header logo-buttons container
-const headerLogoButtonContainer = document.createElement('div');
-headerLogoButtonContainer.classList.add(
-  'header-logo-button-container'
-);
-
-//header main button
-const headerMainButton = document.createElement('button');
-headerMainButton.textContent = 'Main';
-headerMainButton.classList.add('header-buttons');
-
-//header menu button
-const headerMenuButton = document.createElement('button');
-headerMenuButton.textContent = 'Menu';
-headerMenuButton.classList.add('header-buttons');
-
-//header about button
-const headerAboutButton = document.createElement('button');
-headerAboutButton.textContent = 'About';
-headerAboutButton.classList.add('header-buttons');
-
-//header about button
-const headerLocationButton = document.createElement('button');
-headerLocationButton.textContent = 'Location';
-headerLocationButton.classList.add('header-buttons');
-
-//header logo container
-const headerLogo = document.createElement('div');
-headerLogo.classList.add('header-logo-container');
-
-headerContent.appendChild(headerText);
-headerContent.appendChild(headerLogoButtonContainer);
-headerContent.appendChild(headerMiniText);
-
-headerLogoButtonContainer.appendChild(headerMainButton);
-headerLogoButtonContainer.appendChild(headerMenuButton);
-headerLogoButtonContainer.appendChild(headerLogo);
-headerLogoButtonContainer.appendChild(headerAboutButton);
-headerLogoButtonContainer.appendChild(headerLocationButton);
-
-header.appendChild(headerContent);
-
-template.appendChild(header);
-
-////////////////////////////
-//          MAIN          //
-////////////////////////////
-
 const mainContent = document.createElement('div');
 mainContent.classList.add('main-container');
 
-const videoSource = document.createElement('iframe');
-videoSource.setAttribute(
-  'src',
-  'https://www.youtube.com/embed/FIQ2F3T1ydM?autoplay=1'
-);
-// videoSource.setAttribute('width', 'auto');
-// videoSource.setAttribute('height', 'auto');
-videoSource.setAttribute('frameborder', '0');
-videoSource.setAttribute('allow', 'autoplay');
-videoSource.setAttribute('autoplay', '');
+export default function createMainPage() {
+  createMainPageContent();
+}
 
-mainContent.appendChild(videoSource);
-template.appendChild(mainContent);
+(function createHeader() {
+  ////////////////////////////
+  //          HEADER        //
+  ////////////////////////////
+
+  //header
+  const header = document.createElement('header');
+  header.classList.add('header-container');
+
+  //header, content
+  const headerContent = document.createElement('div');
+  headerContent.classList.add('header-content');
+
+  //header welcome to text
+  const headerText = document.createElement('p');
+  headerText.textContent = 'Welcome to';
+
+  //header mini text
+  const headerMiniText = document.createElement('p');
+  headerMiniText.textContent =
+    'Where something good is always cooking.';
+
+  //header logo-buttons container
+  const headerLogoButtonContainer = document.createElement('div');
+  headerLogoButtonContainer.classList.add(
+    'header-logo-button-container'
+  );
+
+  //header main button
+  const headerMainButton = document.createElement('button');
+  headerMainButton.textContent = 'Main';
+  headerMainButton.classList.add('header-buttons');
+
+  //header menu button
+  const headerMenuButton = document.createElement('button');
+  headerMenuButton.textContent = 'Menu';
+  headerMenuButton.classList.add('header-buttons');
+
+  //header about button
+  const headerAboutButton = document.createElement('button');
+  headerAboutButton.textContent = 'About';
+  headerAboutButton.classList.add('header-buttons');
+
+  //header about button
+  const headerLocationButton = document.createElement('button');
+  headerLocationButton.textContent = 'Location';
+  headerLocationButton.classList.add('header-buttons');
+
+  //header logo container
+  const headerLogo = document.createElement('div');
+  headerLogo.classList.add('header-logo-container');
+
+  headerContent.appendChild(headerText);
+  headerContent.appendChild(headerLogoButtonContainer);
+  headerContent.appendChild(headerMiniText);
+
+  headerLogoButtonContainer.appendChild(headerMainButton);
+  headerLogoButtonContainer.appendChild(headerMenuButton);
+  headerLogoButtonContainer.appendChild(headerLogo);
+  headerLogoButtonContainer.appendChild(headerAboutButton);
+  headerLogoButtonContainer.appendChild(headerLocationButton);
+
+  header.appendChild(headerContent);
+
+  template.appendChild(header);
+})();
+
+function createMainPageContent() {
+  ////////////////////////////
+  //          MAIN          //
+  ////////////////////////////
+
+  const videoSource = document.createElement('iframe');
+  videoSource.setAttribute(
+    'src',
+    'https://www.youtube.com/embed/FIQ2F3T1ydM?autoplay=1'
+  );
+  videoSource.setAttribute('frameborder', '0');
+  // videoSource.setAttribute('allow', 'autoplay');
+  // videoSource.setAttribute('autoplay', '');
+
+  mainContent.appendChild(videoSource);
+  template.appendChild(mainContent);
+}
