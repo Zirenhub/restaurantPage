cb81908444e11983d8cee24863dac7fdadb4dccb
Erdinch <erdinchorhan02@gmail.com>
2022-08-17 21:18:48 +0300

    about page text

--- src/app/about-page.js
+++ src/app/about-page.js
@@ -14,7 +14,7 @@ function createAboutPageContent(mainContainer) {
 
   const aboutFirstCol = document.createElement('p');
   aboutFirstCol.textContent =
-    'In the little village where I was born, life moved at a slower pace, yet felt all the richer for it. There, my two uncles were known far and wide for their delicious cooking. They seasoned their zesty chicken using only the freshest herbs and spices. People call them Los Pollos Hermanos: the chicken brothers. Today we carry on their tradition in a manner that would make my uncles proud. The finest ingredients are brought together with love and care, then slow cooked to perfection. Yes, the old ways are still best at Los Pollos Hermanos. But don’t take my word for it. One taste, and you’ll know.';
+    'In the little village where I was born, life moved at a slower pace, yet felt all the richer for it. There, my two uncles were known far and wide for their delicious cooking. They seasoned their zesty chicken using only the freshest herbs and spices. People call them Los Pollos Hermanos: the chicken brothers. ';
   aboutFirstCol.classList.add('about-first-col');
 
   const aboutSecondCol = document.createElement('div');
@@ -23,11 +23,11 @@ function createAboutPageContent(mainContainer) {
   const aboutThirdCol = document.createElement('p');
   aboutThirdCol.classList.add('about-third-col');
   aboutThirdCol.textContent =
-    'In the little village where I was born, life moved at a slower pace, yet felt all the richer for it. There, my two uncles were known far and wide for their delicious cooking. They seasoned their zesty chicken using only the freshest herbs and spices. People call them Los Pollos Hermanos: the chicken brothers. Today we carry on their tradition in a manner that would make my uncles proud. The finest ingredients are brought together with love and care, then slow cooked to perfection. Yes, the old ways are still best at Los Pollos Hermanos. But don’t take my word for it. One taste, and you’ll know.';
+    'Today we carry on their tradition in a manner that would make my uncles proud. The finest ingredients are brought together with love and care, then slow cooked to perfection. Yes, the old ways are still best at Los Pollos Hermanos. But don’t take my word for it. One taste, and you’ll know.';
 
-  aboutContent.appendChild(aboutThirdCol);
-  aboutContent.appendChild(aboutSecondCol);
   aboutContent.appendChild(aboutFirstCol);
+  aboutContent.appendChild(aboutSecondCol);
+  aboutContent.appendChild(aboutThirdCol);
   aboutContainer.appendChild(aboutContent);
   mainContainer.appendChild(aboutContainer);
 }
