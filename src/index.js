0860d8b94cc739b75c2a93ad8c484e5dc4a0eef7
Erdinch <erdinchorhan02@gmail.com>
2022-08-17 19:24:03 +0300

    tabbing function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-import pageLoad from './app/page-load';
+import createMainPage from './app/page-load';
+import createMenuPage from './app/menu-page';
+import createAboutPage from './app/about-page';
 import './style.css';
+
+createMainPage();
+
+const headerTabs = document.querySelectorAll('.header-buttons');
+
+function changeTabs(tabText) {
+  const mainContainer = document.querySelector('.main-container');
+  while (mainContainer.hasChildNodes()) {
+    mainContainer.removeChild(mainContainer.lastChild);
+  }
+  if (tabText === 'Main') {
+    createMainPage();
+  } else if (tabText === 'Menu') {
+    createMenuPage();
+  } else if (tabText === 'About') {
+    createAboutPage();
+  }
+}
+
+headerTabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    let tabText = tab.textContent;
+    changeTabs(tabText);
+  });
+});
